dfb1cbb3da9595652e5120c020640fb6aa21dda2
mayanktripathi4u <mayanktripathi4u@gmail.com>
2024-06-27 17:44:28 -0500

    feat: changes in main.tf to add project

--- infra/main.tf
+++ infra/main.tf
@@ -3,4 +3,5 @@
 resource "google_storage_bucket" "bkt_data_source" {
   name = "bkt_data_source_mt_devops"
   location = "us-east1"
+  project = "proud-climber-421817"
 }
--- infra/provider.tf
+++ infra/provider.tf
@@ -1,3 +1,3 @@
 provider "google" {
-  
+  project = "proud-climber-421817"
 }
